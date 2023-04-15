678bad11e5bfa506c50e099f70f63c1c466a360f
Alaoefyn <121045925+Alaoefyn@users.noreply.github.com>
2023-04-15 13:08:25 +0300

    Update Project3_Question1_Test2.m

--- Project3/Project3_Question1_Test2.m
+++ Project3/Project3_Question1_Test2.m
@@ -1,13 +1,7 @@
-% Affan Selim Kaya 
-% 200709006
-% Computer Engineering (Eng)
-% Introduction to Programming
-% CEN103
-% 2020-2021 1st Somestr
-% Project 3 Question 1/SubFunction2:Test2
-% Asst.Prof. Deniz Dal
+% Alaoefyn
 
-function [output] = AffanSelim_Kaya_200709006_CEN103_Project3_Question1_Test2(num1, num2, num3)
+
+function [output] = Project3_Question1_Test2(num1, num2, num3)
 %Checking if user type a number greater than 9
 %Because we can only use numbers from 0 to 9 to create 2 and larger digit numbers.
 output = (num1 > 9 || num2 > 9 || num3 > 9 );
