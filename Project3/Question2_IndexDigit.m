1a9db5025e864382c6e80775142138961f9a812f
Alaoefyn <121045925+Alaoefyn@users.noreply.github.com>
2023-04-15 13:12:17 +0300

    Update Question2_IndexDigit.m

--- Project3/Question2_IndexDigit.m
+++ Project3/Question2_IndexDigit.m
@@ -1,13 +1,7 @@
-% Affan Selim Kaya 
-% 200709006
-% Computer Engineering (Eng)
-% Introduction to Programming 
-% CEN103
-% 2020-2021 1st Somestr
-% Project 3 Question 2/Subfunction2:IndexDigit
-% Asst.Prof. Deniz Dal
+% Alaoefyn
 
-function [digit] = AffanSelim_Kaya_200709006_CEN103_Project3_Question2_IndexDigit(currentNumber,index)
+
+function [digit] = Project3_Question2_IndexDigit(currentNumber,index)
 %We are getting 'index's digits and checking them
 rest = currentNumber / 10^(index - 1);
 digit = fix(mod(rest,10));
