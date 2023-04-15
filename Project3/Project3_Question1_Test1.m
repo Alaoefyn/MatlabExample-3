7c2e149045c570ebe980f65ad9d97dd755338ef0
Alaoefyn <121045925+Alaoefyn@users.noreply.github.com>
2023-04-15 13:07:58 +0300

    Update Project3_Question1_Test1.m

--- Project3/Project3_Question1_Test1.m
+++ Project3/Project3_Question1_Test1.m
@@ -1,13 +1,6 @@
-% Affan Selim Kaya 
-% 200709006
-% Computer Engineering (Eng)
-% Introduction to Programming
-% CEN103
-% 2020-2021 1st Somestr
-% Project 3 Question 1/SubFunction1:Test1
-% Asst.Prof. Deniz Dal
+%Alaoefyn
 
-function [output] = AffanSelim_Kaya_200709006_CEN103_Project3_Question1_Test1(num1,num2,num3)
+function [output] = Project3_Question1_Test1(num1,num2,num3)
 % Checking if user type a number less than 0
 %Because with a negative number, we cannot create a 2-digit or larger-digit number.
 output = ( 0 > num1 || 0 > num2 || 0 > num3);
